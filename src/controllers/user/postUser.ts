5ff77f841d6fb3036cf228de44ef6ea9f25c0cd4
Saar Davidson <davidsonsaar@gmail.com>
2023-06-09 06:14:52 +0300

    fix post route

--- src/controllers/user/postUser.ts
+++ src/controllers/user/postUser.ts
@@ -2,8 +2,8 @@ import { RequestHandler } from 'express'
 import User from '../../models/User';
 
 const postUser: RequestHandler = async (req, res) => {
-  const { login: { uuid: id }, name, gender, location, email, phone, picture, dob } = req.body;
-  const newUser = new User({ id, name, gender, location, email, phone, picture, dob });
+  const { login: { uuid }, name, gender, location, email, phone, picture, dob } = req.body;
+  const newUser = new User({ uuid, name, gender, location, email, phone, picture, dob });
   await newUser.save();
   res.status(201).json(newUser);
 }
